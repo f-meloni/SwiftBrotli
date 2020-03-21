13007ef736bd61d48f83c22cb3e9b9f773e25c37
Franco Meloni <franco.meloni91@gmail.com>
2020-03-21 12:17:51 +0000

    Offer better API for encoding

--- Tests/SwiftBrotliTests/BrotliJSONEncoderTests.swift
+++ Tests/SwiftBrotliTests/BrotliJSONEncoderTests.swift
@@ -4,13 +4,9 @@ import XCTest
 final class BrotliJSONEncoderTests: XCTestCase {
     func testEncode() throws {
         let encoder = BrotliJSONEncoder()
-        if #available(OSX 10.15, *) {
-            encoder.jsonEncoder.outputFormatting = [.withoutEscapingSlashes, .prettyPrinted]
-        } else {
-            encoder.jsonEncoder.outputFormatting = .prettyPrinted
-        }
         
-        let result: Result<Data, Error> = encoder.encode(brotliDecodedModel)
+        let result: Result<Data, Error>
+        result = encoder.encode(brotliDecodedModel)
         
         XCTAssertEqual(try result.get(), brotliTextCompressedData)
     }
