fccba47d3c3d98e58a356bdbce57218364c74dc5
Franco Meloni <franco.meloni91@gmail.com>
2020-03-21 14:08:00 +0000

    Made API public

--- Sources/SwiftBrotli/BrotliJSONEncoder.swift
+++ Sources/SwiftBrotli/BrotliJSONEncoder.swift
@@ -1,10 +1,10 @@
 import Foundation
 
-struct BrotliJSONEncoder {
+public struct BrotliJSONEncoder {
     private let brotli = Brotli()
     private let jsonEncoder = JSONEncoder()
     
-    func encode<T: Encodable>(_ object: T) -> Result<Data, Error> {
+    public func encode<T: Encodable>(_ object: T) -> Result<Data, Error> {
         #if !os(Linux)
         if #available(tvOS 13.0, OSX 10.15, iOS 13, *) {
             return encode(object, outputFormatting: [.withoutEscapingSlashes])
@@ -16,7 +16,7 @@ struct BrotliJSONEncoder {
         #endif
     }
     
-    func encode<T: Encodable>(_ object: T, outputFormatting: JSONEncoder.OutputFormatting) -> Result<Data, Error> {
+    public func encode<T: Encodable>(_ object: T, outputFormatting: JSONEncoder.OutputFormatting) -> Result<Data, Error> {
         jsonEncoder.outputFormatting = outputFormatting
         return Result { try jsonEncoder.encode(object) }.flatMap { brotli.compress($0, mode: .text).mapError { $0 as Error } }
     }
