db8a168a0aa2b568b25c4d1677d3e8de04223e59
Franco Meloni <franco.meloni91@gmail.com>
2020-03-21 03:00:47 +0000

    Try to make tests consistent between macos and linux

--- Tests/SwiftBrotliTests/BrotliJSONEncoderTests.swift
+++ Tests/SwiftBrotliTests/BrotliJSONEncoderTests.swift
@@ -4,9 +4,9 @@ import XCTest
 final class BrotliJSONEncoderTests: XCTestCase {
     func testEncode() throws {
         let encoder = BrotliJSONEncoder()
+        encoder.jsonEncoder.outputFormatting = .prettyPrinted
         
         let result: Result<Data, Error> = encoder.encode(brotliDecodedModel)
-        
         
         XCTAssertEqual(try result.get(), brotliTextCompressedData)
     }
